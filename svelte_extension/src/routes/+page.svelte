dfc3df22f19ed28eefa7d4a41f492d27c77ee7ac
DFriend01 <devon.fri.2000@gmail.com>
2023-05-21 09:38:11 -0700

    Add images

--- svelte_extension/src/routes/+page.svelte
+++ svelte_extension/src/routes/+page.svelte
@@ -83,22 +83,40 @@
             yes: "This content could contain misinformation.",
             no: "This content appears to be truthful.",
             prompt: `
-                You are an AI assistant that helps consumers identify misinformation on the internet. It is important to note that scams are a form of
-                misinformation, so if you believe that the text is a scam, then it is also misinformation.
-                
-                Detect if the provided text after the triple dash lines is truthful or not. 
+                You are an AI assistant that helps consumers identify misinformation on the internet. I will provide you with some warning signs of
+                misinformation to look out for when you are evaluating your results.
+
+                =============================
+                WARNING SIGNS OF MISINFORMATION
+
+                1. Lack of Credible Sources: Misinformation often lacks references to credible sources or provides unreliable sources that cannot be verified. Look for content supported by reputable organizations, experts, or well-established media outlets.
+                2. Sensational or Clickbait Headlines: Misleading or sensational headlines designed to grab attention and evoke strong emotional responses are common in misinformation. If a headline seems too sensational or exaggerated, it's essential to verify the information from reliable sources.
+                3. Poor Grammar, Spelling, or Formatting: Misinformation pieces often contain noticeable errors in grammar, spelling, or formatting. While mistakes can occur, a high prevalence of such errors may suggest a lack of editorial oversight or professionalism.
+                4. Extreme Biases or One-Sided Perspectives: Misinformation may display extreme biases or present only one side of a story without considering alternative viewpoints. Look for balanced presentations that provide a comprehensive view of the topic.
+                5. Lack of Transparency or Anonymity: Misinformation sources often lack transparency regarding their authors, affiliations, or funding sources. Be skeptical of content that does not provide clear information about the people or organizations behind it.
+                6. Lack of Dates or Outdated Information: Misinformation may lack dates or provide outdated information, making it difficult to assess the relevance and accuracy of the content. Always check for the timeliness of the information before drawing conclusions.
+                7. Emotional Manipulation or Appeal: Misinformation frequently uses emotional appeals to manipulate readers' feelings or provoke strong reactions. Be cautious of content that relies heavily on emotional language or aims to incite anger, fear, or outrage.
+                8. Conspiracy Theories or Unverified Claims: Misinformation often promotes conspiracy theories or unverified claims without substantial evidence. Claims that contradict widely accepted facts or lack credible supporting evidence should be viewed skeptically.
+                9. Lack of Cross-Referencing: Misinformation sources often lack cross-referencing or verification from multiple reliable sources. Look for information that is corroborated by multiple trustworthy sources to increase confidence in its accuracy.
+                10. Echo Chambers or Viral Sharing: Misinformation can spread rapidly within echo chambers or through viral sharing on social media platforms. Be cautious of content that lacks wider exposure or only appears within specific groups or communities.
+                11. Intention to scam: Scams are a form of misinformation because the scammer intends to deceive the victim.
+                               
+                =============================
+                YOUR TASK
+
+                Detect if the provided text after the triple dash lines is misinformation or not. It is very important that you use 
+                the warning signs that I provided you to help you determine if the text is providing misinformation or not. 
 
                 Your answer should include the following:
 
-                1. A "Yes" or "No" statement indicating that the text is misinformation or not. If you cannot tell, then indicate this.
+                1. A "Yes" or "No" statement indicating that the text is misinformation or not. Answer "Yes" if there is misinformation, and answer "No" if there is no misinformation.
                 2. You should explain why you came to your conclusion, specifically referencing the originally provided text.
                 3. Your response should be no longer than 3 sentences.
+                4. Do not complete the rest of the text yourself and include it in your answer.
 
                 ---
 
             `
-            
-            //prompt: "Detect if the following text contains false information. Explain your reasoning. Must start the response with yes or no. If neither, say N/A.\n\n",
         },
         {
             yes: "This content could be Ai generated.",
@@ -112,10 +130,9 @@
 
                 1. Warning signs of Unusual or Inconsistent Language: AI-generated text might contain language patterns that seem unnatural or inconsistent. It could exhibit an odd choice of words, unusual sentence structures, or inconsistent grammar.
                 2. Warning signs of Lack of Context or Coherence: AI-generated text may struggle to maintain a coherent and logical flow of information. It might provide irrelevant or nonsensical responses, fail to address specific questions, or exhibit abrupt topic shifts.
-                3. Warning signs of Overuse of Buzzwords or Jargon: AI models are trained on vast amounts of text data, and sometimes they tend to overuse buzzwords, clichés, or jargon. If you notice an excessive and unnecessary use of such language, it could be an indication of AI-generated content.
-                4. Warning signs of Absence of Personal Experience or Emotion: AI lacks personal experiences and emotions, so the generated text might lack subjective opinions, personal anecdotes, or emotional nuances that a human writer would typically include.
-                5. Warning signs of Uncommon or Invented Facts: AI-generated text might present information that is unusual, obscure, or even factually incorrect. It could generate statistics, quotes, or historical events that do not exist or are inaccurate.
-                6. Warning signs of Consistency and Speed: AI models can generate vast amounts of content quickly and consistently. If you observe a high volume of content that appears to be generated in a short period and lacks human errors or variations, it could suggest AI involvement.
+                3. Warning signs of Absence of Personal Experience or Emotion: AI lacks personal experiences and emotions, so the generated text might lack subjective opinions, personal anecdotes, or emotional nuances that a human writer would typically include.
+                4. Warning signs of Uncommon or Invented Facts: AI-generated text might present information that is unusual, obscure, or even factually incorrect. It could generate statistics, quotes, or historical events that do not exist or are inaccurate.
+                5. Warning signs of Consistency and Speed: AI models can generate vast amounts of content quickly and consistently. If you observe a high volume of content that appears to be generated in a short period and lacks human errors or variations, it could suggest AI involvement.
 
                 =============================
                 YOUR TASK
